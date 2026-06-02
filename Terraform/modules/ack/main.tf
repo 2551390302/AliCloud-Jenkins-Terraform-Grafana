95894def0f30c236c812031f01b1696b9810d316
李勇 <2551390302@qq.com>
2026-06-03 00:01:47 +0800

    terraform fmt

--- Terraform/modules/ack/main.tf
+++ Terraform/modules/ack/main.tf
@@ -3,19 +3,17 @@ resource "alicloud_cs_managed_kubernetes" "this" {
   worker_vswitch_ids = var.worker_vswitch_ids
   pod_vswitch_ids    = var.pod_vswitch_ids
 
-  worker_instance_types = var.worker_instance_types
-  worker_number         = var.worker_number
-  new_nat_gateway       = var.new_nat_gateway
-  resource_group_id     = var.resource_group_id != "" ? var.resource_group_id : null
+  worker_instance_types       = var.worker_instance_types
+  worker_number               = var.worker_number
+  new_nat_gateway             = var.new_nat_gateway
+  resource_group_id           = var.resource_group_id != "" ? var.resource_group_id : null
+  
+  # 节点系统盘配置（虽然已标记为 deprecated，但仍可使用）
+  worker_system_disk_category = var.worker_system_disk_category
+  worker_system_disk_size     = var.worker_system_disk_size
 
   is_enterprise_security_group = true
   install_cloud_monitor        = true
-
-  # 节点池配置
-  default_node_pool {
-    system_disk_category = var.worker_system_disk_category
-    system_disk_size     = var.worker_system_disk_size
-  }
 
   tags = var.tags
 }
