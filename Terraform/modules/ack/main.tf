91f0e36c4f3460e845577c0bd2b276dde151dee6
李勇 <2551390302@qq.com>
2026-06-03 00:26:55 +0800

    terraform fmt

--- Terraform/modules/ack/main.tf
+++ Terraform/modules/ack/main.tf
@@ -1,7 +1,7 @@
 resource "alicloud_cs_managed_kubernetes" "this" {
-  name               = var.cluster_name
-  worker_vswitch_ids = var.worker_vswitch_ids
-  pod_vswitch_ids    = var.pod_vswitch_ids
+  name           = var.cluster_name
+  vswitch_ids    = var.worker_vswitch_ids
+  pod_vswitch_ids = var.pod_vswitch_ids
 
   worker_instance_types = var.worker_instance_types
   worker_number         = var.worker_number
@@ -9,7 +9,6 @@ resource "alicloud_cs_managed_kubernetes" "this" {
   resource_group_id     = var.resource_group_id != "" ? var.resource_group_id : null
 
   is_enterprise_security_group = true
-  install_cloud_monitor        = true
 
   tags = var.tags
 }
